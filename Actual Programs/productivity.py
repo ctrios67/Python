91c4a0a804fd8b7a0b438be837d0fb22c191dc37
Chris Rios <ctrios67@gmail.com>
2017-03-13 12:09:06 -0400

    Adding finishes touches to productivity.py; mainly pointing to actual host file.

--- Actual Programs/productivity.py
+++ Actual Programs/productivity.py
@@ -3,11 +3,12 @@
 
 import time
 from datetime import datetime as dt
+import os
 # '/etc/hosts'
 # r'C:\Windows\System32\drivers\etc'
 # 'C:\\Windows\\System32\\drivers\\etc'
 
-hosts_path = '/etc/hosts/'
+hosts_path = '/etc/hosts'
 hosts_temp = 'hosts'
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com','facebook.com',
@@ -16,9 +17,9 @@ website_list = ['www.facebook.com','facebook.com',
              'www.instagram.com', 'instagram.com']
 
 while True:
-    if(dt(dt.now().year, dt.now().month, dt.now().day, 8) < (dt(dt.now().year, dt.now().month, dt.now().day, 16))):
+    if(dt(dt.now().year, dt.now().month, dt.now().day, 9) < (dt(dt.now().year, dt.now().month, dt.now().day, 17))):
         print('Working Hours...')
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content = file.read()
             for website in website_list:
                 if(website in content):
@@ -27,5 +28,12 @@ while True:
                     file.write(redirect + ' ' + website+'\n')
             print(content)
     else:
+        with open(hosts_path,'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if(not any(website in line for website in website_list)):
+                    file.write(line)
+            file.truncate()
         print('Fun hours...')
     time.sleep(5)
