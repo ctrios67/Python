249b2fd44c91e82a0ed8789aee9b321d3366e032
Chris Rios <ctrios67@gmail.com>
2017-03-13 10:28:35 -0400

    Added blocked sites to host file portion.

--- Actual Programs/productivity.py
+++ Actual Programs/productivity.py
@@ -8,6 +8,7 @@ from datetime import datetime as dt
 # 'C:\\Windows\\System32\\drivers\\etc'
 
 hosts_path = '/etc/hosts/'
+hosts_temp = 'hosts'
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com','facebook.com',
              'www.twitter.com', 'twitter.com',
@@ -17,6 +18,14 @@ website_list = ['www.facebook.com','facebook.com',
 while True:
     if(dt(dt.now().year, dt.now().month, dt.now().day, 8) < (dt(dt.now().year, dt.now().month, dt.now().day, 16))):
         print('Working Hours...')
+        with open(hosts_temp,'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if(website in content):
+                    pass
+                else:
+                    file.write(redirect + ' ' + website+'\n')
+            print(content)
     else:
         print('Fun hours...')
     time.sleep(5)
