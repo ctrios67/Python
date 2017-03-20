f6672f029679045b61d690039c71f85e1178cd65
Chris Rios <ctrios67@gmail.com>
2017-03-19 20:26:12 -0400

    Finishing touches on backend and frontend of Book Store program

--- Actual Programs/BookDB/frontend.py
+++ Actual Programs/BookDB/frontend.py
@@ -17,6 +17,22 @@ Delete
 Close
 '''
 
+def get_selected_row(event):
+    # Global variable needed because in the call to the backend delete function, we can't pass an event.
+    global selected_tuple
+    # This returns a tuple of the form (#,), without the 0 it returns everything
+    index = list1.curselection()[0]
+    selected_tuple = list1.get(index)
+    # The following fills the entry windows when an item is selected
+    e1.delete(0,END)
+    e1.insert(END, selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END, selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END, selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END, selected_tuple[4])
+
 def view_command():
     # From index 0 of Listbox to end
     list1.delete(0,END)
@@ -34,8 +50,17 @@ def add_command():
     list1.delete(0,END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+def update_command():
+    # This is wrong because we sending the exact same thing back to the DB
+    # backend.update(selected_tuple[0], selected_tuple[1], selected_tuple[2], selected_tuple[3], selected_tuple[4])
+    backend.update(selected_tuple[0],title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
 
 window = Tk()
+
+window.wm_title('BookStore')
 
 l1 = Label(window, text='Title')
 l1.grid(row=0, column=0)
@@ -73,6 +98,9 @@ sb1.grid(row=2, column=2, rowspan=6)
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
+# Binds an action to button's event
+# Here we choose when an item is selected in the list box
+list1.bind('<<ListboxSelect>>', get_selected_row)
 
 #Needs wrapper functions
 b1 = Button(window,text='View All', width=12, command=view_command)
@@ -84,13 +112,13 @@ b2.grid(row=3, column=3)
 b3 = Button(window,text='Add Entry', width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window,text='Update Entry', width=12)
+b4 = Button(window,text='Update Entry', width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window,text='Delete Entry', width=12)
+b5 = Button(window,text='Delete Entry', width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window,text='Close', width=12)
+b6 = Button(window,text='Close', width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
