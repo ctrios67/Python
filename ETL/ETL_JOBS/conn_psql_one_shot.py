6cf2ef8254874a354e2d677633452599897df9f5
Christian Rios <christian.rios.917@gmail.com>
2021-05-04 21:44:31 -0400

    Looked at psql example from google (source in comments), trying to combine it with SQL in a separate file

---
+++ ETL/ETL_JOBS/conn_psql_one_shot.py
@@ -0,0 +1,47 @@
+# This gist contains a direct connection to a local PostgreSQL database
+# called "suppliers" where the username and password parameters are "postgres"
+
+# This code is adapted from the tutorial hosted below:
+# http://www.postgresqltutorial.com/postgresql-python/connect/
+
+import psycopg2
+import os
+import glob2
+
+# Since this is a "connect to DB in one shot to do DB stuff" file, get the SQL statement from the ETL_METADATA folder before connecting to Postgres
+# TODO: Fix this! This is initial approach is very stupid but I just want to make sure I remember how this works, it's been awhile.
+etl_metadata_dir = os.getcwd().replace("ETL_JOBS", "ETL_METADATA")
+
+# Lazily get the SQL file so we can read from it
+sql_file = ""
+for i in glob2.glob(etl_metadata_dir + "*.sql"):
+    sql_file = i.replace(os.join(etl_metadata_dir, os.sep()), "")
+
+sql_to_execute = ""
+
+# Establish a connection to the database by creating a cursor object
+# The PostgreSQL server must be accessed through the PostgreSQL APP or Terminal Shell
+
+# conn = psycopg2.connect("dbname=suppliers port=5432 user=postgres password=postgres")
+
+# Or:
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="postgres",
+    user="postgres",
+    password="password",  # Please hack me :)
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# A sample query of all data from the "vendors" table in the "suppliers" database
+cur.execute("""SELECT * FROM vendors""")  # change this my dude
+query_results = cur.fetchall()
+print(query_results)
+
+# Close the cursor and connection to so the server can allocate
+# bandwidth to other requests
+cur.close()
+conn.close()
--- ETL/ETL_METADATA/create_etl_data_source_tracker.sql
+++ ETL/ETL_METADATA/create_etl_data_source_tracker.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dev_etl.etl_data_source_tracker
 (
-    data_source_id                          VARCHAR(4000)
-    ,etl_processing_id                      VARCHAR2(4000)
-    ,processing_date                        DATE
+    data_source_id                          VARCHAR2(4000)
+    ,data_etl_processing_id                 VARCHAR2(4000)
+    ,data_processing_date                   DATE
     ,data_source_name                       VARCHAR2(4000)
     ,data_source_raw_table                  VARCHAR2(4000)
     ,data_source_stg_table                  VARCHAR2(4000)
