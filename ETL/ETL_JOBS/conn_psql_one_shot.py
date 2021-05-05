bf2d4bb8aba6b3f1bf5376fa9f3b66c53b8f804e
Christian Rios <christian.rios.917@gmail.com>
2021-05-04 22:19:19 -0400

    As rough as it is, it works! At least according to PGadmin. I see read/writes, but no tables/schemas which is a DBA/grant issue

--- ETL/ETL_JOBS/conn_psql_one_shot.py
+++ ETL/ETL_JOBS/conn_psql_one_shot.py
@@ -14,20 +14,22 @@ etl_metadata_dir = os.getcwd().replace("ETL_JOBS", "ETL_METADATA")
 
 # Lazily get the SQL file(s) so we can read from it, and store how many files there are
 sql_files = []
-for i in glob2.glob(etl_metadata_dir + "*.sql"):
+for i in glob2.glob(os.path.join(etl_metadata_dir, "*.sql")):
     sql_files.append(i)
 
 num_of_sql_files = len(sql_files)
 
 # Open the files we got, and bring the SQL inside as one long string.
 sql_to_execute_1 = ""
-with open(sql_file[0], "r") as sql_getter:
-    sql_to_execute_1 = sql_getter.readlines()
+with open(sql_files[0], "r") as sql_getter:
+    sql_to_execute_1 = sql_getter.read()
 
 sql_to_execute_2 = ""
-with open(sql_file[num_of_sql_files - 1], "r") as sql_getter:
-    sql_to_execute_2 = sql_getter.readlines()
+with open(sql_files[num_of_sql_files - 1], "r") as sql_getter:
+    sql_to_execute_2 = sql_getter.read()
 
+print(sql_to_execute_1)
+print(sql_to_execute_2)
 # Establish a connection to the database by creating a cursor object
 # The PostgreSQL server must be accessed through the PostgreSQL APP or Terminal Shell
 
@@ -47,10 +49,14 @@ cur = conn.cursor()
 
 # A sample query of all data from the "vendors" table in the "suppliers" database
 cur.execute(sql_to_execute_1)
-query_results = cur.fetchall()
-print(query_results)
+# query_results = cur.fetchall()
+# print(query_results)
 
 cur.execute(sql_to_execute_2)
+# query_results = cur.fetchall()
+# print(query_results)
+
+cur.execute("""SELECT * FROM dev_etl.etl_data_source_tracker""")
 query_results = cur.fetchall()
 print(query_results)
 
